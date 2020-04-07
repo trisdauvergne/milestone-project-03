91ec68616ef777185e2c07f7e5de01850b6409cb
trisdauvergne <tristan.dauvergne@gmail.com>
2020-04-07 15:31:19 +0000

    200407 - 17:30 docs: created templates folder with base html

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'wt_database'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+print(os.environ.get("MONGO_URI"))
 
 
 @app.route('/')
 def hello():
     return 'Hello World! Again...'
+
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
