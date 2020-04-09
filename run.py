abf7e919716e78a804d5d2674b241c7b37160556
trisdauvergne <tristan.dauvergne@gmail.com>
2020-04-09 08:56:51 +0000

    200409 - 10:56 feat: added form to additem page which adds to database

--- run.py
+++ run.py
@@ -17,47 +17,83 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
-@app.route('/')
+coll = mongo.db.wt_collection
+
+@app.route("/")
 def hello():
-    return render_template("welcomepage.html")
+    return render_template('welcomepage.html')
 
 
-@app.route('/test')
+@app.route("/test")
 def test():
-    return render_template("test.html",
+    return render_template('test.html',
     collection=mongo.db.wt_collection.find())
 
 
-@app.route('/mylists')
+@app.route("/mylists")
 def mylists():
-    return render_template("mylists.html",
+    return render_template('mylists.html',
     collection=mongo.db.wt_collection.find())
 
 
-@app.route('/listitems')
+@app.route("/listitems")
 def listitems():
-    return render_template("listitems.html",
+    return render_template('listitems.html',
     collection=mongo.db.wt_collection.find())
 
 
-@app.route('/additem')
+@app.route("/additem", methods=["GET", "POST"])
 def additem():
-    return render_template("additem.html")
+
+    if request.method == 'GET':
+        return render_template('additem.html')
+
+    if request.method == 'POST':
+        additem_listname = request.form['additem_listname']
+        additem_productlink = request.form['additem_productlink']
+        additem_brandname = request.form['additem_brandname']
+        additem_productcat = request.form['additem_productcat']
+        additem_price = request.form['additem_price']
+        additem_urgency = request.form['additem_urgency']
+
+        newly_added_item = {'additem_listname': additem_listname,
+                            'additem_productlink': additem_productlink,
+                            'additem_brandname': additem_brandname,
+                            'additem_productcat': additem_productcat,
+                            'additem_price': additem_price,
+                            'additem_urgency': additem_urgency}
+
+        coll.insert_one(newly_added_item)
+
+    return render_template('listitems.html')
+
+@app.route("/create")
+def create():
+    new_list_item = {'brand': 'COS',
+                     'type': 'Shirt',
+                     'price': '250SEK',
+                     'description': 'White oversized t-shirt'}
+
+    mongo.db.wt_collection.insert_one(new_list_item)
+
+    return render_template('create.html', document=new_list_item)
 
 
-@app.route('/edititem')
+@app.route("/edititem")
 def edititem():
-    return render_template("edititem.html")
+    return render_template('edititem.html')
 
 
-@app.route('/newlist')
+@app.route("/newlist")
 def newlist():
-    return render_template("newlist.html")
+    return render_template('newlist.html')
 
 
-@app.route('/editlist')
+@app.route("/editlist")
 def editlist():
-    return render_template("editlist.html")
+    return render_template('editlist.html')
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
