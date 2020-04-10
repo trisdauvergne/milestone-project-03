fc37f2df7f99f8e9d6a12905f4019885b487badb
trisdauvergne <tristan.dauvergne@gmail.com>
2020-04-10 12:01:09 +0000

    200410 - 14:00 feat: updated edit item function

--- run.py
+++ run.py
@@ -130,6 +130,27 @@ def edititem(item_id):
     _item = mongo.db.wt_listitems.find_one({'_id': ObjectId(item_id)})
     print(_item)
 
+    if request.method == 'POST':
+        list_name = request.form['list_name']
+        product_link = request.form['product_link']
+        brand_name = request.form['brand_name']
+        product_type = request.form['product_type']
+        item_description = request.form['item_description']
+        item_price = request.form['item_price']
+        need_rating = request.form['need_rating']
+
+        updated_item = {'list_name': list_name,
+                        'product_link': product_link,
+                        'brand_name': brand_name,
+                        'product_type': product_type,
+                        'item_description': item_description,
+                        'item_price': item_price,
+                        'need_rating': need_rating}
+
+        mongo.db.wt_listitems.update({'_id': ObjectId(item_id)}, updated_item)
+
+        return redirect(url_for('listitems'))
+
     return render_template('edititem.html', 
                             item_info=_item, 
                             list=mongo.db.wt_collection.find())
